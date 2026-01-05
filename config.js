f2a3afc8cf728bae23b3507175a1cac8cba74c06
raviyunanto <ravi.yunanto@gmail.com>
2026-01-05 10:37:44 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const CONFIG = {
     name: "@raviyunanto",       // Nama Utama
     
     // === STATUS KETERSEDIAAN ===
-    isOpenToWork: true,         // true = Hijau (Open), false = Merah (Busy)
+    isOpenToWork: false,         // true = Hijau (Open), false = Merah (Busy)
 
     // === BIO (EFEK KETIK) ===
     typingTexts: [
